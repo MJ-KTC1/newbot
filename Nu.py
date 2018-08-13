5274c2d9ece4d6946defc58f29219abc88f734cb
༄۞ꪶꪶꪣꪫꪙุ۞࿐ <40826871+nunu2@users.noreply.github.com>
2018-08-13 08:27:38 +0700

    Update Nu.py

--- Nu.py
+++ Nu.py
@@ -14,64 +14,106 @@ botStart = time.time()
 #==============================================================================#
 #line = LINE()
 #line = LINE("เมล","พาส")
-line = LINE()
-line.log("Auth Token : " + str(line.authToken))
-line.log("Timeline Token : " + str(line.tl.channelAccessToken))
+#line = LINE()
+#line.log("Auth Token : " + str(line.authToken))
+#line.log("Timeline Token : " + str(line.tl.channelAccessToken))
 
 #~~~~~~~~~~~~~~~~~~~~คิกผี~~~~~~~~~~~~~~~~~~~~`
-km = LINE('')
-km.log("Auth Token : " + str(ke.authToken))
-km.log("Timeline Token : " + str(ke.tl.channelAccessToken))
-
-
+line = LINE()
+ki1 = LINE()
+ki2 = LINE()
+ki3 = LINE()
+ki4 = LINE()
+ki5 = LINE()
+ki7 = LINE()
+ki8 = LINE()
+ki9 = LINE()
+ki10 = LINE()
+km = LINE()
+#km.log("Auth Token : " + str(ke.authToken))
+#km.log("Timeline Token : " + str(ke.tl.channelAccessToken))
 print ("Login Succes")
-
 lineMID = line.profile.mid
 lineProfile = line.getProfile()
 lineSettings = line.getSettings()
 
-kiMID = ki.profile.mid
-kiProfile = ki.getProfile()
-kiSettings = ki.getSettings()
+ki1MID = ki1.profile.mid
+ki1Profile = ki1.getProfile()
+ki1Settings = ki1.getSettings()
 
-kkMID = kk.profile.mid
-kkProfile = kk.getProfile()
-kkSettings = kk.getSettings()
+ki2MID = ki2.profile.mid
+ki2Profile = ki2.getProfile()
+ki2Settings = ki2.getSettings()
 
-kcMID = kc.profile.mid
-kcProfile = kc.getProfile()
-kcSettings = kc.getSettings()
+ki3MID = ki3.profile.mid
+ki3Profile = ki3.getProfile()
+ki3Settings = ki3.getSettings()
 
-keMID = kc.profile.mid
-keProfile = kc.getProfile()
-keSettings = kc.getSettings()
+ki4MID = ki4.profile.mid
+ki4Profile = ki4.getProfile()
+ki4Settings = ki4.getSettings()
+
+ki5MID = ki5.profile.mid
+ki5Profile = ki5.getProfile()
+ki5Settings = ki5.getSettings()
+
+ki6MID = ki6.profile.mid
+ki6Profile = ki6.getProfile()
+ki6Settings = ki6.getSettings()
+
+ki7MID = ki7.profile.mid
+ki7Profile = ki7.getProfile()
+ki7Settings = ki7.getSettings()
+
+ki8MID = ki8.profile.mid
+ki8Profile = ki8.getProfile()
+ki8Settings = ki8.getSettings()
+
+ki9MID = ki9.profile.mid
+ki9Profile = ki9.getProfile()
+ki9Settings = ki9.getSettings()
+
+ki10MID = ki10.profile.mid
+ki10Profile = ki10.getProfile()
+ki10Settings = ki10.getSettings()
 
 kmMID = km.profile.mid
 kmProfile = km.getProfile()
-kmSettings = km.getSettings()
-
+km0Settings = km.getSettings()
 oepoll = OEPoll(km)
-oepoll = OEPoll(ke)
-oepoll = OEPoll(kc)
-oepoll = OEPoll(kk)
-oepoll = OEPoll(ki)
+oepoll = OEPoll(ki10)
+oepoll = OEPoll(ki9)
+oepoll = OEPoll(ki8)
+oepoll = OEPoll(ki7)
+oepoll = OEPoll(ki6)
+oepoll = OEPoll(ki5)
+oepoll = OEPoll(ki4)
+oepoll = OEPoll(ki3)
+oepoll = OEPoll(ki2)
+oepoll = OEPoll(ki1)
 oepoll = OEPoll(line)
 readOpen = codecs.open("read.json","r","utf-8")
 settingsOpen = codecs.open("temp.json","r","utf-8")
 read = json.load(readOpen)
 settings = json.load(settingsOpen)
-Rfu = [line,ki,kk,kc,ke]
-Exc = [ki,kk,kc,ke]
+Rfu = [line,ki1,ki2,ki3,ki4,ki5,ki6,ki7,ki8,ki9,ki10,km]
+Exc = [ki1,ki2,ki3,ki4,ki5,ki6,ki7,ki8,ki9,ki10,km]
 lineMID = line.getProfile().mid
-kiMID = ki.getProfile().mid
-kkMID = kk.getProfile().mid
-kcMID = kc.getProfile().mid
-kcMID = ke.getProfile().mid
+ki1MID = ki1.getProfile().mid
+ki2MID = ki2.getProfile().mid
+ki3MID = ki3.getProfile().mid
+ki4MID = ki4.getProfile().mid
+ki5MID = ki5.getProfile().mid
+ki6MID = ki6.getProfile().mid
+ki7MID = ki7.getProfile().mid
+ki8MID = ki8.getProfile().mid
+ki9MID = ki9.getProfile().mid
+ki10MID = ki10.getProfile().mid
 kmMID = km.getProfile().mid
 bot1 = line.getProfile().mid
-RfuBot=[lineMID,kiMID,kkMID,kcMID,keMID,kmMID]
-Family=["u8f4b03bd2f026a30dbff351d5a08dfc3",lineMID,kiMID,kkMID,kcMID,keMID,kmMID]
-admin=['u8f4b03bd2f026a30dbff351d5a08dfc3',lineMID]
+RfuBot=[lineMID,ki1MID,ki2MID,ki3MID,ki4MID,ki5MID,ki6MID,ki7MID,ki8MID,ki9MID,ki10MID,kmMID]
+Family=["ud3a6bfda60a956cca0f58f2a14bae808",lineMID,ki1MID,ki2MID,ki3MID,ki4MID,ki5MID,ki6MID,ki7MID,ki8MID,ki9MID,ki10MID,kmMID]
+admin=['ud3a6bfda60a956cca0f58f2a14bae808',lineMID]
 RfuFamily = RfuBot + Family
 
 protectname = []
